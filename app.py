255d82c83b457c5e2d1b1a7c506dc13aaa2a0f8c
Mariane Costa <84878947+marianecosta@users.noreply.github.com>
2023-10-31 10:41:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-from datetime import date
 import streamlit.components.v1 as components
 
 arquivo_csv = "database-tradingview.csv"  
